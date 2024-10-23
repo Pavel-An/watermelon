1749f547e1bea2d56e6ff574b469190f14eba10c
Pavel Androsovich <b00025597@lotus.asb.by>
2024-10-23 14:01:55 +0300

    add validates to Project model, update migration 20241023055441_create_projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description
+  validates :status, numericality: { only_integer: true}
+
   has_many :project_members
   has_many :users, through: :project_members
+
+  enum status: { active: 1, archived: 2 }
 end
