5aa0d5ef12156c87e18ecc1c3f257b7d0d8fb5aa
Pavel Androsovich <b00025597@lotus.asb.by>
2024-10-23 14:17:40 +0300

    update migration 20241022135139_devise_create_users.rb, column admin type integer change to boolean, uncomment block Confirmable

--- db/migrate/20241022135139_devise_create_users.rb
+++ db/migrate/20241022135139_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.integer :admin, null: false, default: "0"
+      t.boolean :admin, null: false, default: false
 
       ## Recoverable
       # t.string   :reset_password_token
@@ -23,10 +23,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
