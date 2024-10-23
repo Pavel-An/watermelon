a5561747a6a2586e382534c0c119a6baab8f4cf4
Pavel Androsovich <b00025597@lotus.asb.by>
2024-10-23 15:04:38 +0300

    delete .irb_history, correct models as in style guide

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -1,9 +1,11 @@
 class ProjectMember < ApplicationRecord
-  validates :project_id, numericality: { only_integer: true}
-  validates :user_id, numericality: { only_integer: true}
+  belongs_to :project
+  belongs_to :user
+
+  validates :project_id, numericality: { only_integer: true }
+  validates :user_id, numericality: { only_integer: true }
   validates :project_id, uniqueness: { scope: :user_id }
   validates :user_id, uniqueness: { scope: :project_id }
 
-  belongs_to :project
-  belongs_to :user
+
 end
