d876f81349790955513a99683f017a92406faff8
Pavel Androsovich <b00025597@lotus.asb.by>
2024-10-24 08:47:58 +0300

    add enum to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,8 @@
 class Project < ApplicationRecord
-  enum status: { active: 1, archived: 2 }
+  enum status: { active: 1, close: 2, archived: 3 }
 
   validates :name, presence: true, length: { maximum: 100 }
-  # validates :description
-  validates :status, numericality: { only_integer: true }
+  validates :status, presence: true
 
   has_many :project_members
   has_many :users, through: :project_members
