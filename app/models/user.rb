afdf00b53917fd3cd8f14c289295f8400ced2767
pavel <pael88@mail.ru>
2024-10-28 09:30:39 +0300

    add mailer config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable, :recoverable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :validatable
 
   enum role: { user: 0, admin: 1 }
 
