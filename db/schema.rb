696d4541bb161400d74200c13931392d28092a09
pavel <pael88@mail.ru>
2024-10-28 11:04:47 +0300

    add confirm mail

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_23_060425) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
