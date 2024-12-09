8a81367c890dea0e4157988ee0ea9b65268247d2
Pavel Androsovich <b00025597@lotus.asb.by>
2024-12-09 10:03:19 +0300

    add gem activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  include Pundit::Authorization
+  
   allow_browser versions: :modern
   around_action :switch_locale
   before_action :turbo_frame_request_variant
